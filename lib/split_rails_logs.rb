ec7402546498806103475c2f49b92ed0d45c9d17
nickbrowne <nickbrowne@users.noreply.github.com>
2019-03-22 14:49:17 +1100

    Implementation

---
+++ lib/split_rails_logs.rb
@@ -0,0 +1,47 @@
+require "active_support"
+require "stringio"
+require "fileutils"
+
+class SplitRailsLogs
+  # Write out logs for each example
+  def self.for_all(example)
+    capture_logs_for(example) do |path, string|
+      write_logs(path, string)
+    end
+  end
+
+  # Write out logs only for each failed example
+  def self.for_failed(example)
+    capture_logs_for(example) do |path, string, failed|
+      write_logs(path, string) if failed
+    end
+  end
+
+  private_class_method def self.capture_logs_for(example)
+    raise "SplitRailsLogs - Must initialize Rails application first" unless defined?(Rails) && Rails.initialized?
+
+    # reusing the io and logger instances each time prevents
+    # runaway memory usage
+    @io ||= StringIO.new
+    @logger ||= Logger.new(@io).tap do |logger|
+      Rails.logger.extend(ActiveSupport::Logger.broadcast(logger))
+    end
+
+    # beause we're reusing the same io and logger
+    # we need to truncate and rewind the io each time
+    @io.truncate(0)
+    @io.rewind
+
+    example.call
+
+    log_file_suffix = "_line_#{example.metadata[:line_number]}.test.log"
+    log_path = Rails.root.join("log").join(example.metadata[:file_path].sub(".rb", log_file_suffix))
+
+    yield log_path, @io.string, example.exception
+  end
+
+  private_class_method def self.write_logs(path, string)
+    FileUtils.mkdir_p(File.dirname(path))
+    File.write(path, string)
+  end
+end
---
+++ spec/split_rails_logs_spec.rb
@@ -0,0 +1,48 @@
+require "split_rails_logs"
+require "pathname"
+require "logger"
+
+RSpec.describe SplitRailsLogs do
+  let(:example) { double(call: nil, metadata: { file_path: "spec/example_spec.rb", line_number: 123 }) }
+  let(:expected_pathname) { Pathname.new("foo/log/spec/example_spec_line_123.test.log") }
+
+  before do
+    stub_const("Rails", double(
+      root: Pathname.new("foo"),
+      logger: Logger.new(nil),
+      initialized?: true,
+    ))
+  end
+
+  describe ".for_all" do
+    it "writes logs for a successful example" do
+      expect(example).to receive(:exception).and_return false
+      expect(File).to receive(:write).with(expected_pathname, "")
+
+      SplitRailsLogs.for_all(example)
+    end
+
+    it "writes logs for a failed example" do
+      expect(example).to receive(:exception).and_return true
+      expect(File).to receive(:write).with(expected_pathname, "")
+
+      SplitRailsLogs.for_all(example)
+    end
+  end
+
+  describe ".for_failed" do
+    it "does not write logs for a successful example" do
+      expect(example).to receive(:exception).and_return false
+      expect(File).to_not receive(:write)
+
+      SplitRailsLogs.for_failed(example)
+    end
+
+    it "writes logs for a failed example" do
+      expect(example).to receive(:exception).and_return true
+      expect(File).to receive(:write).with(expected_pathname, "")
+
+      SplitRailsLogs.for_failed(example)
+    end
+  end
+end
